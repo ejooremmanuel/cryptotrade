ac1c4c5e3e238f2dd6922eeb5b269d590a927144
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-11-01 00:38:50 +0100

    new route

--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -6,4 +6,7 @@ const {
 } = require("../../controllers/user/user.controllers");
 router.get("/dashboard", isLoggedIn, Dashboard);
 router.get("/wallet", isLoggedIn, Wallet);
+router.get("/dashboard?", (req, res) => {
+  console.log(req.query);
+});
 module.exports = router;
